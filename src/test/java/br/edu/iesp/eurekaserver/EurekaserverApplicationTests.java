3a0ce6b471a2b4559628ed9a70aa2dccfad3a412
Rodrigo Barbosa <rodrigo.barba.88@gmail.com>
2020-06-09 20:06:02 -0300

    first commit to repository.

---
+++ src/test/java/br/edu/iesp/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iesp.eurekaserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
